2332360578b3f24d584f6908f5acbb06b057a813
Betelihem <Betelihem>
2022-02-07 11:52:43 -0800

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener ("DOMContentLoaded", () => {
+  let form = document.querySelector ('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault ()
+    buildToDo (document.getElementById ('new-task-description').value)
+    form.reset ()
+  })
 });
+
+function buildToDo (todo) {
+  console.log (todo)
+  let ul = document.createElement ('ul')
+  let btn = document.createElement ('button')
+  btn.addEventListener ('click', handleDelete)
+  btn.textContent = 'x'
+  ul.textContent = `${todo} `
+  ul.appendChild (btn)
+  document.querySelector ('#list').appendChild(ul)
+  console.log (ul)
+}
+
+function handleDelete (e) {
+  e.target.parentNode.remove ()
+}
